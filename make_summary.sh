ef5c31c353d0173db60d41a77d3cdd987e948b43
grandrea <graziadei.a@gmail.com>
2021-12-06 22:11:01 +0100

    script to make summary from a set of folders analysed with plot_AF_all.py

---
+++ make_summary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+grep model ./*/*/*.csv > summary.txt
+
+grep -v "model,ptm,iptm,plddt,confrank" summary.txt > summary.csv
+
+rm summary.txt
+
+sed -i 's/model_statistics.csv:/,/g' summary.csv
+sed -i 's/\/,/,/g' summary.csv
+sed -i 's/^.*\///g' summary.csv
+
+echo "run,model,ptm,iptm,plddt,confrank" > a
+cat a summary.csv > b
+mv b summary.csv
+
+
