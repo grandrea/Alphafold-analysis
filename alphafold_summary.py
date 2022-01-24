9b3cb40c154a913d8105e65952ca65b0f4ee4825
grandrea <graziadei.a@gmail.com>
2022-01-24 16:26:22 +0100

    split summary plots, add ptm vs iptm

--- alphafold_summary.py
+++ alphafold_summary.py
@@ -14,13 +14,13 @@ import matplotlib.pyplot as plt
 
 data_frame = pd.read_csv('summary.csv')
 
-data_frame_top = data_frame[data_frame['iptm']>0.670]
+data_frame_top = data_frame[data_frame['iptm']>0.75]
 data_frame_top = data_frame_top.sort_values(by=['iptm'], ascending=False)
 data_frame_top = data_frame_top.drop_duplicates(subset = ["run"])
 
 plt.clf()
-fig, axs = plt.subplots(ncols=6,
-                        figsize = (16, 4))
+fig, axs = plt.subplots(ncols=3,
+                        figsize = (16, 3))
 
 sns.histplot(data_frame['ptm'], bins=20, ax=axs[0])
 axs[0].title.set_text('pTM score')
@@ -32,29 +32,61 @@ axs[1].title.set_text('ipTM score')
 sns.histplot(data_frame['plddt'], bins=20, ax=axs[2])
 axs[2].title.set_text('plddt')
 
-sns.scatterplot(data_frame['ptm'], data_frame['plddt'], ax=axs[3])
-axs[3].title.set_text('plddt vs pTM')
+
+plt.suptitle('Alphfold run summary')
+plt.savefig('summary_histograms.png', dpi=300)
+
+plt.clf()
+fig, axs = plt.subplots(ncols=3,
+                        figsize = (16, 3))
+sns.scatterplot(data_frame['ptm'], data_frame['plddt'], ax=axs[0])
+axs[0].title.set_text('plddt vs pTM')
+
+sns.scatterplot(data_frame['iptm'], data_frame['plddt'], ax=axs[1])
+axs[1].title.set_text('plddt vs ipTM')
+
+sns.scatterplot(data_frame['ptm'], data_frame['iptm'], ax=axs[2])
+axs[2].title.set_text('ipTM vs pTM')
 
 
-sns.scatterplot(data_frame['iptm'], data_frame['plddt'], ax=axs[4])
-axs[4].title.set_text('plddt vs ipTM')
+plt.suptitle('Alphfold run summary')
+plt.savefig('summary_scatter.png', dpi=300)
 
-sns.scatterplot(data_frame_top['iptm'], data_frame_top['plddt'], ax=axs[5])
+plt.clf()
+fig, axs = plt.subplots(ncols=2,
+                        figsize = (12, 2))
+
+sns.scatterplot(data_frame_top['iptm'], data_frame_top['plddt'], ax=axs[0])
 # add annotations one by one with a loop
-axs[5].title.set_text('best hits by iptm')
+axs[0].title.set_text('best hits by iptm/plddt')
+#axs[5].set_xlim([0.8, 1.0])
 # add annotations one by one with a loop
 for line in range(0,data_frame_top.shape[0]):
-     axs[5].text(data_frame_top.iptm.iloc[line]+0.001, 
+     axs[0].text(data_frame_top.iptm.iloc[line]+0.001, 
               data_frame_top.plddt.iloc[line],
               data_frame_top.run.iloc[line], 
               horizontalalignment='left',
               size='small', 
               color='black')
+
+sns.scatterplot(data_frame_top['iptm'], data_frame_top['ptm'], ax=axs[1])
+# add annotations one by one with a loop
+axs[1].title.set_text('best hits by iptm/ptm')
+#axs[5].set_xlim([0.8, 1.0])
+# add annotations one by one with a loop
+for line in range(0,data_frame_top.shape[0]):
+     axs[1].text(data_frame_top.iptm.iloc[line]+0.001, 
+              data_frame_top.ptm.iloc[line],
+              data_frame_top.run.iloc[line], 
+              horizontalalignment='left',
+              size='small', 
+              color='black')
      
+plt.savefig('summary_best_scatter.png', dpi=300)
 
-plt.suptitle('Alphfold run summary')
-
-plt.savefig('summary.png')
+plt.clf()
+sns.jointplot(data=data_frame, x="ptm", y="iptm")
+plt.savefig('ptm_iptm_summary.png', dpi=300)
 
 
 sorted_data_frame = data_frame.sort_values(by=['iptm'], ascending=False)
