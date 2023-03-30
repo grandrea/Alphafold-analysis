6aaebd98648e0df1776b0d9f59e6ca2bef1a59e0
grandrea <44289027+grandrea@users.noreply.github.com>
2023-03-30 10:09:19 +0200

    cosmetics, remove unused variable

--- plot_AF_all.py
+++ plot_AF_all.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set_theme()
+import seaborn as sns
+sns.set_theme()
 import pickle
 from Bio import SeqIO
 from itertools import accumulate
@@ -8,7 +9,7 @@ import glob
 import numpy as np
 import pandas as pd
 
-#fasta file with input sequence must be in result directory
+# fasta file with input sequence must be in result directory
 input_sequence_name = glob.glob('*fasta')[0]
 
 #plot title as in fasta file
@@ -20,7 +21,7 @@ except:
 
 try:
     input_sequence = SeqIO.to_dict(SeqIO.parse(input_sequence_name, 'fasta'))
-#handle homomultimers with identical fasta headers
+# handle homomultimers with identical fasta headers
 except ValueError:
     with open(input_sequence_name) as original, open('corrected.fasta', 'w') as corrected:
         records = SeqIO.parse(original, 'fasta')
@@ -31,11 +32,11 @@ except ValueError:
     corrected.close()
     input_sequence = SeqIO.to_dict(SeqIO.parse('corrected.fasta', 'fasta'))
 
-#pickle files coming out of AF run
+# pickle files coming out of AF run
 file_list = glob.glob('*_multimer*.pkl')
 file_list = sorted(file_list)
 
-#finished run will produce a .json file with the model ranking 
+# finished run will produce a .json file with the model ranking
 
 try:
     json1_file = open('ranking_debug.json')
@@ -43,12 +44,12 @@ try:
     model_stats = json.loads(json1_str)
     json1_file.close()
     model_ranking = model_stats.get('order')
-except FileNotFoundError: #plot an incomplete run 
+except FileNotFoundError:  # plot an incomplete run
     model_ranking = []
 
 
 
-PAE_list = []
+
 
 width_list = []
 
@@ -56,7 +57,7 @@ list_ranking = {}
 
 statistics_list = []
 
-#read pkl, match model number with rank using json file
+# read pkl, match model number with rank using json file
 for index, file_name in enumerate(file_list):
     d = pickle.load(open(file_name,'rb'))
     model_name = file_name.replace('.pkl', '')
@@ -72,11 +73,11 @@ for index, file_name in enumerate(file_list):
                             d.get('iptm'),
                             np.mean(d.get('plddt')),
                             d.get('ranking_confidence')])
-    PAE_list.append(d)
+
 
 width_list.append(0.2)
 
-#check protein lengths in input files for PAE plot
+# check protein lengths in input files for PAE plot
 sequence_lengths = []
 
 for item in input_sequence.keys():
@@ -85,18 +86,18 @@ for item in input_sequence.keys():
     sequence_lengths.append(sequence_length)
     
 
-#figure out positions for black lines delimiting proteins in PAE plot
+# figure out positions for black lines delimiting proteins in PAE plot
 line_position_accumulated = accumulate(sequence_lengths)
 line_positions = []
 for item in line_position_accumulated:
     line_positions.append(item)
 
 
-#write stats file--------
+# write stats file--------
 model_stats = pd.DataFrame(statistics_list, 
-             columns = ['model', 'ptm', 'iptm', 'plddt', 'confrank'])
-model_stats.to_csv('model_statistics.csv',  index=False )
-#PAE plot -------------------
+                           columns = ['model', 'ptm', 'iptm', 'plddt', 'confrank'])
+model_stats.to_csv('model_statistics.csv',  index=False)
+# PAE plot -------------------
 
 output_name = str('predicted_alignment_error.png')
 
@@ -116,8 +117,8 @@ for file_name in file_list:
                 cmap=palette, 
                 ax=axs[plot_number], 
                 cbar=False, 
-                vmin= 0, 
-                vmax = 30)
+                vmin=0,
+                vmax=30)
 #    axs[plot_number].yaxis.set_major_locator(mticker.MaxNLocator(5))
 #    axs[plot_number].xaxis.set_major_locator(mticker.MaxNLocator(5))
     if plot_number == 0:
@@ -130,7 +131,7 @@ for file_name in file_list:
                                         '\n iptm: ' +
                                         str(ipTM)))
     
-    #add black lines delimiting the two proteins
+    # add black lines delimiting the two proteins
     for element in line_positions:
         axs[plot_number].vlines(element,
                                 ymin=0,
@@ -148,12 +149,12 @@ fig.suptitle(str('Predicted alignment error ' + protein_names_for_title))
 plt.savefig(output_name)
 
 
-#pLDDT plot------------------------------
+# pLDDT plot------------------------------
 
 output_name = str('pLDDT.png')
 
 
-#palette = sns.light_palette("#2ecc71", as_cmap=True, reverse=True)
+# palette = sns.light_palette("#2ecc71", as_cmap=True, reverse=True)
 
 fig, axs = plt.subplots(ncols=len(file_list),
                         figsize = (6*len(file_list), 3))
@@ -184,7 +185,7 @@ fig.colorbar(axs[0].collections[0], cax=axs[-1])
 fig.suptitle(str('plddt ' + protein_names_for_titile))
 plt.savefig(output_name)
 
-#MSA plot------------------------------
+# MSA plot------------------------------
 if "features.pkl" in glob.glob("*"):
     features = pickle.load(open("features.pkl", "rb"))
     plt.close()
